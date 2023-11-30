601a2b15266fb0be12212e83b0cedcc6aca095c6
omarjalis <152468027+omarjalis@users.noreply.github.com>
2023-11-30 10:47:11 +0530

    Add files via upload

---
+++ customscriptextension.ps1
@@ -0,0 +1,13 @@
+# Enable Windows Defender settings
+Set-MpPreference -DisableRealtimeMonitoring $false -Force
+Set-MpPreference -CloudBlockLevel 0 -Force
+Set-MpPreference -MAPSReporting 1 -Force
+
+# Uninstall ClamWin antivirus if it exists
+$clamWinUninstallPath = "C:\Program Files (x86)\ClamWin\unins000.exe"
+if (Test-Path $clamWinUninstallPath) {
+    Start-Process -FilePath $clamWinUninstallPath -ArgumentList "/SILENT" -Wait
+    Write-Output "ClamWin antivirus has been uninstalled."
+} else {
+    Write-Output "ClamWin antivirus is not installed."
+}
