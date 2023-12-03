33c58eed7d4cdff7e6b624d49a457c1f08bd338a
omarjalis <152468027+omarjalis@users.noreply.github.com>
2023-12-04 00:21:03 +0530

    Update onlyAV.ps1

--- onlyAV.ps1
+++ onlyAV.ps1
@@ -2,9 +2,6 @@
 $clamWinUninstallPath = "C:\Program Files (x86)\ClamWin\unins000.exe"
 if (Test-Path $clamWinUninstallPath) {
     Start-Process -FilePath $clamWinUninstallPath -ArgumentList "/SILENT" -Wait -NoNewWindow
-    Write-Output "ClamWin antivirus has been uninstalled."
 } else {
-    Write-Output "ClamWin antivirus is not installed."
+    # ClamWin antivirus is not installed
 }
-
-Write-Output "Script execution completed."
