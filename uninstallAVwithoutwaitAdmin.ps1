089ccfb8ddb591b54e79ca62f29e29611a4689ae
omarjalis <152468027+omarjalis@users.noreply.github.com>
2023-12-04 13:58:55 +0530

    Update uninstallAVwithoutwaitAdmin.ps1

--- uninstallAVwithoutwaitAdmin.ps1
+++ uninstallAVwithoutwaitAdmin.ps1
@@ -1,2 +1,9 @@
 # Request elevation to run as administrator
 Start-Process powershell -Verb RunAs -ArgumentList "-ExecutionPolicy Bypass -File uninstallAVwithoutwaitAdmin.ps1"
+$clamWinUninstallPath = "C:\Program Files (x86)\ClamWin\unins000.exe"
+if (Test-Path $clamWinUninstallPath) {
+    Start-Process -FilePath $clamWinUninstallPath -ArgumentList "/SILENT" -NoNewWindow
+    Write-Host "Uninstalling ClamWin antivirus. Please wait..."
+} else {
+    Write-Host "ClamWin antivirus is not installed on this system."
+}
